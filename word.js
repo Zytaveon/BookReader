6e6f5c6e537911c78cdc876aa5e006ea73b2e4f0
mcmandaa <mcmandaa@uni.edu>
2024-02-29 13:56:16 -0600

    Added two functions, getNewWord and getRestWords. Makes the while loop cleaner. They do work. They will ignore spaces and newlines, but not any other characters so any and any? will be two different words right now.

--- word.js
+++ word.js
@@ -9,48 +9,82 @@ function wordCounter(){
     let newWord;
     let restWords = new String(input);
 
-    //Should get rid of whitespace at the end
-    restWords = restWords.trim();
-
-    /* This while loop works
+    //This while loop works
     while(!restWords == ""){
 
-        if(restWords.indexOf(" ") == -1){
-            newWord = restWords;
-            restWords = "";
-        }
+       newWord = getNewWord(restWords);
+       restWords = getRestWords(restWords);
 
-        else{
-            newWord = restWords.slice(0, restWords.indexOf(" "));
-            restWords = restWords.slice(restWords.indexOf(" ") + 1);
-        }
-
-
+        console.log(newWord);
+        console.log(restWords);
     }
-    */
+    
 
-    newWord = getNewWord(restWords)
+    
 
-    console.log(newWord);
-    console.log(restWords);
 
-    newWord = getNewWord(restWords)
-
-    console.log(newWord);
-    console.log(restWords);
-
-    //window.alert("We made it past the while");
+    window.alert("We made it past the while");
 
 }
 
 function getNewWord(listOfWords){
 
     let restWords = new String(listOfWords);
+    restWords = restWords.trim();
     let newWord;
 
-    newWord = restWords.slice(0, restWords.indexOf(" "));
-    restWords = restWords.slice(restWords.indexOf(" ") + 1);
+    if(restWords.indexOf(" ") === -1){
 
+        if(restWords.indexOf("\n") == -1){
+            newWord = restWords;
+            return newWord;
+        }
+
+        else{
+            newWord = restWords.slice(0, restWords.indexOf("\n"));
+            return newWord;
+        }
+    }
+
+    if(restWords.indexOf("\n") == -1){
+        newWord = restWords.slice(0, restWords.indexOf(" "));
+        return newWord;
+    }
+
+    if(restWords.indexOf(" ") < restWords.indexOf("\n")){
+        newWord = restWords.slice(0, restWords.indexOf(" "));
+        return newWord;
+    }
+
+    newWord = restWords.slice(0, restWords.indexOf("\n"));
     return newWord;
+    
+}
 
+function getRestWords(listOfWords){
+
+    let restWords = new String(listOfWords);
+    let newList;
+
+    if(restWords.indexOf(" ") == -1){
+        if(restWords.indexOf("\n") == -1){
+            return "";
+        }
+
+        newList = restWords.slice(restWords.indexOf("\n") + 1);
+        return newList.trim();
+    }
+
+    if(restWords.indexOf("\n") == -1){
+        newList = restWords.slice(restWords.indexOf(" ") + 1);
+        return newList.trim();
+    }
+
+    if(restWords.indexOf(" ") < restWords.indexOf("\n")){
+        newList = restWords.slice(restWords.indexOf(" ") + 1);
+        return newList.trim();
+    }
+
+    newList = restWords.slice(restWords.indexOf("\n") + 1);
+    return newList.trim();
 }
